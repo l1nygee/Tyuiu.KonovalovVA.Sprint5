e764ea145776d562fff098131907870663144d96
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-12-25 23:09:04 +0500

    Task7

--- Tyuiu.KonovalovVA.Sprint5.Task7.V6/Program.cs
+++ Tyuiu.KonovalovVA.Sprint5.Task7.V6/Program.cs
@@ -10,13 +10,14 @@ namespace Tyuiu.KonovalovVA.Sprint5.Task7.V6
             Console.WriteLine("***************************************************************************");
 
             string path = @"C:\Users\volod\source\repos\Tyuiu.KonovalovVA.Sprint5\Tyuiu.KonovalovVA.Sprint5.Task7.V6\bin\Debug\net8.0\InPutDataFileTask7V6.txt";
+            string pathSave = @"C:\Users\volod\source\repos\Tyuiu.KonovalovVA.Sprint5\Tyuiu.KonovalovVA.Sprint5.Task7.V6\bin\Debug\net8.0\OutPutDataFileTask7V6.txt";
             Console.WriteLine("Данные находятся в файле: " + path);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            string res = ds.LoadDataAndSave(path);
+            string res = ds.LoadDataAndSave(pathSave);
             Console.WriteLine(res);
             Console.ReadKey();
         }
