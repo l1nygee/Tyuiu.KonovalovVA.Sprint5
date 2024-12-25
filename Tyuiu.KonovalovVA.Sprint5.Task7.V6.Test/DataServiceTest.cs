3630c345e23745da93f4cb5317ceaeb0d05a7c12
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-12-26 01:13:21 +0500

    Task7

--- Tyuiu.KonovalovVA.Sprint5.Task7.V6.Test/DataServiceTest.cs
+++ Tyuiu.KonovalovVA.Sprint5.Task7.V6.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.KonovalovVA.Sprint5.Task7.V6.Test
             string path = $@"C:\Users\volod\source\repos\Tyuiu.KonovalovVA.Sprint5\Tyuiu.KonovalovVA.Sprint5.Task7.V6\bin\Debug\net8.0\OutPutDataFileTask7V6.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileexists = fileInfo.Exists;
-            Assert.IsTrue(fileexists);
+            Assert.AreEqual(true,fileexists);
         }
     }
     
