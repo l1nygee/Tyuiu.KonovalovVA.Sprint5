ca6bdd0d4a88e86b81ad54f0ac9fdfa69f5daddb
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-12-25 22:14:26 +0500

    Task7

--- Tyuiu.KonovalovVA.Sprint5.Task7.V6.Test/DataServiceTest.cs
+++ Tyuiu.KonovalovVA.Sprint5.Task7.V6.Test/DataServiceTest.cs
@@ -7,12 +7,10 @@ namespace Tyuiu.KonovalovVA.Sprint5.Task7.V6.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\DataSprint5\InPutDataFileTask7V6.txt";
-
+            string path = "C:\\DataSprint5\\Sprint5Task7\\InPutDataFileTask7V6.txt";
             FileInfo fileInfo = new FileInfo(path);
-            bool fileExists = fileInfo.Exists;
-            bool wait = true;
-            Assert.AreEqual(wait, fileExists);
+            bool fileexists = fileInfo.Exists;
+            Assert.IsTrue(fileexists);
         }
     }
     
